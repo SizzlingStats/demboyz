2e75619f426948f5dff2431d4ea47c6d6f8a41a4
Jordan Cristiano <jordan.cristi@gmail.com>
2015-05-15 18:05:28 -0400

    Fixed incorrect macro concat operation

--- demboyz/netmessages/nethandlers.h
+++ demboyz/netmessages/nethandlers.h
@@ -21,30 +21,30 @@ struct SourceGameContext
 #define DECLARE_NET_HANDLERS(msgname) \
     namespace NetHandlers \
     { \
-        bool msgname##_BitRead_Internal(bf_read& bitbuf, SourceGameContext& context, NetMsg::##msgname* data); \
-        bool msgname##_BitWrite_Internal(bf_write& bitbuf, const SourceGameContext& context, NetMsg::##msgname* data); \
-        bool msgname##_JsonRead_Internal(JsonRead& jsonbuf, SourceGameContext& context, NetMsg::##msgname* data); \
-        bool msgname##_JsonWrite_Internal(JsonWrite& jsonbuf, const SourceGameContext& context, NetMsg::##msgname* data); \
-        void msgname##_ToString_Internal(std::ostringstream& out, NetMsg::##msgname* data); \
+        bool msgname##_BitRead_Internal(bf_read& bitbuf, SourceGameContext& context, NetMsg::msgname* data); \
+        bool msgname##_BitWrite_Internal(bf_write& bitbuf, const SourceGameContext& context, NetMsg::msgname* data); \
+        bool msgname##_JsonRead_Internal(JsonRead& jsonbuf, SourceGameContext& context, NetMsg::msgname* data); \
+        bool msgname##_JsonWrite_Internal(JsonWrite& jsonbuf, const SourceGameContext& context, NetMsg::msgname* data); \
+        void msgname##_ToString_Internal(std::ostringstream& out, NetMsg::msgname* data); \
         inline bool msgname##_BitRead(bf_read& bitbuf, SourceGameContext& context, void* data) \
         { \
-            return msgname##_BitRead_Internal(bitbuf, context, reinterpret_cast<NetMsg::##msgname*>(data)); \
+            return msgname##_BitRead_Internal(bitbuf, context, reinterpret_cast<NetMsg::msgname*>(data)); \
         } \
         inline bool msgname##_BitWrite(bf_write& bitbuf, const SourceGameContext& context, void* data) \
         { \
-            return msgname##_BitWrite_Internal(bitbuf, context, reinterpret_cast<NetMsg::##msgname*>(data)); \
+            return msgname##_BitWrite_Internal(bitbuf, context, reinterpret_cast<NetMsg::msgname*>(data)); \
         } \
         inline bool msgname##_JsonRead(JsonRead& jsonbuf, SourceGameContext& context, void* data) \
         { \
-            return msgname##_JsonRead_Internal(jsonbuf, context, reinterpret_cast<NetMsg::##msgname*>(data)); \
+            return msgname##_JsonRead_Internal(jsonbuf, context, reinterpret_cast<NetMsg::msgname*>(data)); \
         } \
         inline bool msgname##_JsonWrite(JsonWrite& jsonbuf, const SourceGameContext& context, void* data) \
         { \
-            return msgname##_JsonWrite_Internal(jsonbuf, context, reinterpret_cast<NetMsg::##msgname*>(data)); \
+            return msgname##_JsonWrite_Internal(jsonbuf, context, reinterpret_cast<NetMsg::msgname*>(data)); \
         } \
         inline void msgname##_ToString(std::ostringstream& out, void* data) \
         { \
-            msgname##_ToString_Internal(out, reinterpret_cast<NetMsg::##msgname*>(data)); \
+            msgname##_ToString_Internal(out, reinterpret_cast<NetMsg::msgname*>(data)); \
         } \
     }
 

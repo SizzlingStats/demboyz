3c4c786283008ec5cad90bbb031eeff9deba69df
Jordan Cristiano <jordan.cristi@gmail.com>
2015-05-15 20:46:09 -0400

    Removed unused var in svc_updatestringtable

--- demboyz/netmessages/svc_updatestringtable.h
+++ demboyz/netmessages/svc_updatestringtable.h
@@ -9,7 +9,6 @@ namespace NetMsg
     struct SVC_UpdateStringTable
     {
         uint32_t tableID;
-        bool twoOrMoreChangedEntries;
         uint16_t numChangedEntries;
         uint32_t dataLengthInBits;
         std::unique_ptr<uint8_t[]> data;

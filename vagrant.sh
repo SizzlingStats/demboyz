c9a8d07522b9f7f518211a88b3f9f9cdabe65b13
Jordan Cristiano <jordan.cristi@gmail.com>
2016-04-27 00:53:43 -0400

    Removed unnecessary vagrant setup

--- vagrant.sh
+++ vagrant.sh
@@ -4,12 +4,6 @@
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 
-# Remove X11 cruft
-sudo apt-get autoremove -y
-
-# Keep things up to date
-sudo apt-get upgrade -y
-
 packages=""; # Start with nothing; can re-order rest to heart's content
 
 packages="$packages build-essential"
@@ -22,7 +16,3 @@ sudo apt-get install -y $packages
 # Why doesn't clang do this for us?
 sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.6 10
 sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.6 10
-
-# Build it!
-cd /vagrant/premake
-./gmake.sh && { cd gmake; make; }

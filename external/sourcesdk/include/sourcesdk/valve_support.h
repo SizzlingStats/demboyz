d8b3b56822e84e74af018e88ff6832cde15ceee2
Jordan Cristiano <jordan.cristi@gmail.com>
2018-02-13 20:47:22 -0500

    Removed c++11 usage from sourcesdk project.

--- external/sourcesdk/include/sourcesdk/valve_support.h
+++ external/sourcesdk/include/sourcesdk/valve_support.h
@@ -7,16 +7,16 @@
 #include <string.h>
 #include "vector.h"
 
-using uint64 = std::uint64_t;
-using uint32 = std::uint32_t;
-using uint16 = std::uint16_t;
-using uint8 = std::uint8_t;
-using int64 = std::int64_t;
-using int32 = std::int32_t;
-using int16 = std::int16_t;
-using int8 = std::int8_t;
-using uint = unsigned int;
-using byte = char;
+typedef std::uint64_t uint64;
+typedef std::uint32_t uint32;
+typedef std::uint16_t uint16;
+typedef std::uint8_t uint8;
+typedef std::int64_t int64;
+typedef std::int32_t int32;
+typedef std::int16_t int16;
+typedef std::int8_t int8;
+typedef unsigned int uint;
+typedef char byte;
 
 #if defined(_M_IX86)
 #define __i386__ 1

a968c9fe89d3360e4bb592abb1264d8c0c45fd6a
Jordan Cristiano <jordan.cristi@gmail.com>
2018-02-14 20:07:36 -0500

    Removed update/upgrade and building from vagrant setup.

--- vagrant.sh
+++ vagrant.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 
-# Keep things up to date
-sudo apt-get update
-sudo apt-get upgrade -y
-
 # Install g++ dependencies 
 sudo apt-get install -y build-essential g++-4.8 g++-4.8-multilib
-
-# Build it!
-cd /vagrant/premake
-./gmake.sh && { cd gmake; make; }

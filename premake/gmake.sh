53fe7bdc9d4e163ad746cb428ffd52f019bf7a63
J.P <stapmoshun@gmail.com>
2024-08-01 03:37:04 -0400

    add workflow

--- premake/gmake.sh
+++ premake/gmake.sh
@@ -6,7 +6,7 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Do something under Linux platform
   ./premake5_linux gmake --file=premake5.lua
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
   # Do something under Windows NT platform
   echo "windows? don't use this"
 fi
